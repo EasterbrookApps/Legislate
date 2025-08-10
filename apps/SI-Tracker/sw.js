9ced7d092a7a83efe89ce505576c4b047a1a00db
EasterbrookApps <daneasterbrook1@gmail.com>
2025-08-10 17:23:32 +0100

    Add files via upload

---
+++ apps/SI-Tracker/sw.js
@@ -0,0 +1,6 @@
+
+const CACHE='si-live-plus-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match('./'))))});
