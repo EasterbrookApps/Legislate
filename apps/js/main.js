c442ab0dd0d4856c438de4cf3c4c6e6b85a43f46
EasterbrookApps <daneasterbrook1@gmail.com>
2025-08-20 08:07:12 +0100

    Delete apps directory

--- apps/js/main.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener('DOMContentLoaded', async ()=>{
-  setupBoardSVG(); setupAdmin(); setupDice(); initPlayers(4); renderPlayersUI();
-  await loadBoardConfig(); await loadDecks();
-  setInterval(()=>{ const f=1+Math.floor(Math.random()*6); $('#floating-die').className='die floating p'+f; }, 8000);
-  startGame();
-});
