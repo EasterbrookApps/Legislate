cd38e38f917687b91df45546102c8e25d44d6bd7
EasterbrookApps <daneasterbrook1@gmail.com>
2025-08-24 07:45:36 +0100

    Delete apps/legislate directory

--- apps/legislate/js/main.js
+++
@@ -1,6 +0,0 @@
-
-window.addEventListener('DOMContentLoaded', async ()=>{
-  setupBoardSVG(); setupAdmin(); setupDice(); initPlayers(4); renderPlayersUI();
-  await loadBoardConfig(); await loadDecks();
-  renderTokens();
-});
