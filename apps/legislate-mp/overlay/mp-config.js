acf4f4f64d18680b29a7eb5863494201c2f6f033
EasterbrookApps <daneasterbrook1@gmail.com>
2025-09-17 09:26:49 +0100

    Update mp-config.js

--- apps/legislate-mp/overlay/mp-config.js
+++ apps/legislate-mp/overlay/mp-config.js
@@ -1,3 +1,7 @@
-// If you have a global window.firebaseConfig already on the page, this will reuse it.
-// Otherwise, create overlay/mp-config.js from mp-config.sample.js with your Firebase config.
-window.MP_CONFIG = window.MP_CONFIG || (window.firebaseConfig ? { firebase: window.firebaseConfig } : null);
+  firebase: {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT",
+    appId: "YOUR_APP_ID",
+  }
+};
