20b8af2ee87826f87e4a1e141720149ee066bdd5
EasterbrookApps <daneasterbrook1@gmail.com>
2025-09-17 09:47:57 +0100

    Update mp-config.js

--- apps/legislate-mp/overlay/mp-config.js
+++ apps/legislate-mp/overlay/mp-config.js
@@ -1,7 +1,8 @@
+window.MP_CONFIG = {
   firebase: {
     apiKey: "AIzaSyAaT34wisvMkSOdJMaWlzsxwL_dUCFHejQ",
     authDomain: "legislateboardgame.firebaseapp.com",
     projectId: "legislateboardgame",
-    appId: "1:624315105777:web:0c0839ec74b1de442d4450",
+    appId: "1:624315105777:web:0c0839ec74b1de442d4450"
   }
 };
